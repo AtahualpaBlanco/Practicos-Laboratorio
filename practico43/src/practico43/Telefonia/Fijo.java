cf670811ba840507de3680ecee9a5da285987009
ULP <ULP@10.62.201.67>
2017-10-26 19:33:47 -0300

    agrege cel, fijo, tel

---
+++ practico43/src/practico43/Telefonia/Fijo.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practico43.Telefonia;
+
+/**
+ *
+ * @author ULP
+ */
+public class Fijo {
+    
+    private String linea;
+    
+    public String getLinea()
+    {
+    return linea;
+    }
+    
+    public void setLinea(String linea)
+    {
+    this.linea=linea;
+    }
+    
+    
+    
+}
